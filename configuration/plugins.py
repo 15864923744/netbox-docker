595377ae62aa3e8287c9c7a1661d9d48c8ab4714
Sun <1247690100@qq.com>
2026-03-24 15:04:55 +0800

    Update plugins.py

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -11,3 +11,20 @@
 #     ADD YOUR SETTINGS HERE
 #   }
 # }
+
+PLUGINS = [
+    "netbox_diode_plugin",
+]
+
+PLUGINS_CONFIG = {
+    "netbox_diode_plugin": {
+        # Diode gRPC target for communication with Diode server
+        "diode_target_override": "grpc://localhost:8080/diode",
+
+        # Username associated with changes applied via plugin
+        "diode_username": "diode-ingest",
+
+        # netbox-to-diode client_secret created during diode bootstrap.
+        "netbox_to_diode_client_secret": "iR23FR393k4JPUpUyM5p5i05YccuOlECX8tWtiNtk0="
+    },
+}
